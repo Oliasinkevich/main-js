b15d6b2e5cf05d298cace808759ccebcfcf3e5a8
Oliasinkevich <olga.sinkevich89@gmail.com>
2023-06-22 03:06:01 +0300

    JSON: 2 workshop & 2 homework

---
+++ 5 JSON/2 workshop/data.js
@@ -0,0 +1,29 @@
+const dataProducts = `
+[
+    {
+        "name": "MANGO  PEOPLE  T-SHIRT",
+        "img": "img/product1.jpg",
+        "price": "300",
+        "color": "Red",
+        "size": "Xl",
+        "quantity": "2"	 	
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "img": "img/product2.jpg",
+        "price": "52",
+        "color": "Black",
+        "size": "Xl",
+        "quantity": "1"
+    },
+    {
+        "name": "ZARA WOMEN SWEETSHOT",
+        "img": "img/product3.jpg",
+        "price": "117",
+        "color": "Green",
+        "size": "M",
+        "quantity": "3"
+    }
+]
+`
+

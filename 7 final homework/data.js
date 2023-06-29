ccdec6e71ab7f04ec98560487bc7d43b6b7e2d6a
Oliasinkevich <olga.sinkevich89@gmail.com>
2023-06-29 17:02:23 +0300

    Final  homework

---
+++ 7 final homework/data.js
@@ -0,0 +1,58 @@
+const dataProducts = `
+[
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "img": "img/store_card1.png",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbite of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "color": "Red",
+        "size": "Xl",
+        "quantity": ""		
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "img": "img/store_card2.png",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbite of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "color": "Red",
+        "size": "Xl",
+        "quantity": ""	
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "img": "img/store_card3.png",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbite of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "color": "Red",
+        "size": "Xl",
+        "quantity": ""	
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "img": "img/store_card4.png",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbite of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "color": "Red",
+        "size": "Xl",
+        "quantity": ""	
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "img": "img/store_card5.png",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbite of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "color": "Red",
+        "size": "Xl",
+        "quantity": ""	
+    },
+    {
+        "name": "ELLERY X M'O CAPSULE",
+        "img": "img/store_card6.png",
+        "description": "Known for her sculptural takes on traditional tailoring, Australian arbite of cool Kym Ellery teams up with Moda Operandi.",
+        "price": "52.00",
+        "color": "Red",
+        "size": "Xl",
+        "quantity": ""	
+    }
+]
+`
